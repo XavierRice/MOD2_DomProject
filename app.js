2c82d55d620d5d15e511bd0b2f549afecd58ced3
xavierrice <xavier.rice@gmail.com>
2023-06-26 11:32:32 -0400

    added some pictures-just to mess around with the css

---
+++ app.js
@@ -0,0 +1,8 @@
+const test = console.log
+const body = document.querySelector("body")
+const form = document.querySelector("form")
+const name = document.getElementById("name")
+const price = document.getElementById("price")
+const radioInput = document.getElementById("stock")
+const pStock = document.getElementsByClassName("stock")
+test(pStock)
